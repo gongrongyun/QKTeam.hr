b5cc8dfddba109a26e0d990a495dadc55420c3f7
KB-mfl <37804131+KB-mfl@users.noreply.github.com>
2019-03-06 22:20:56 +0800

    textarea

--- resources/views/introduce.blade.php
+++ resources/views/introduce.blade.php
@@ -4,36 +4,49 @@
     <div class="container" id="container">
         @if(isset($member))
             <div class="cardBox">
-                <div class="headerBox">
-                    <p>个人报名信息</p>
+                <div class="headerBox" style="text-align: center;font-size: 30px">
+                    <p><strong>个人报名信息</strong></p>
+                    </br>
                 </div>
-                <div class="bodyBox">
-                    <p><strong>姓名：</strong>{{$member->name}}</p>
-                    <p><strong>生日：</strong>{{$member->birthday}}</p>
-                    <p><strong>学号：</strong>{{$member->studentId}}</p>
-                    <p><strong>学院：</strong>{{$member->college}}</p>
-                    <p><strong>专业：</strong>{{$member->profession}}</p>
-                    <p><strong>手机号：</strong>{{$member->phoneNumber}}</p>
-                    <p><strong>邮箱：</strong>{{$member->email}}</p>
-                    <p><strong>扣扣：</strong>{{$member->qq}}</p>
-                    <p><strong>性别：</strong>{{$member->gender}}</p>
+                <div class="bodyBox" style="padding: 30px">
+                    <p><strong>姓名：</strong>{{$member->name}}</p></br>
+                    <p><strong>生日：</strong>{{$member->birthday}}</p></br>
+                    <p><strong>学号：</strong>{{$member->studentId}}</p></br>
+                    <p><strong>学院：</strong>{{$member->college}}</p></br>
+                    <p><strong>专业：</strong>{{$member->profession}}</p></br>
+                    <p><strong>手机号：</strong>{{$member->phoneNumber}}</p></br>
+                    <p><strong>邮箱：</strong>{{$member->email}}</p></br>
+                    <p><strong>扣扣：</strong>{{$member->qq}}</p></br>
+                    <p><strong>性别：</strong>{{$member->gender}}</p></br>
                     <?php if($member->selfIntroduction!=null):?>
-                    <p><strong>自我介绍：</strong><?php echo e($member->selfIntroduction); ?></p>
+                    <h><strong>自我介绍：</strong></h>
+                    </br>
+                    <div class="textarea"><?php echo e($member->selfIntroduction); ?></div>
+                    </br>
                     <?php endif;?>
                     <?php if($member->blog!=null):?>
                     <p><strong>个人博客：</strong><?php echo e($member->blog); ?></p>
+                    </br>
                     <?php endif;?>
                     <?php if($member->github账号!=null):?>
                     <p><strong>github：</strong><?php echo e($member->github); ?></p>
+                    </br>
                     <?php endif;?>
+                    </br>
                     <?php if($member->skills!='null'):?>
                     <p><strong>技能：</strong><?php echo e(substr($member->skills,1,strlen($member->skills)-2)); ?></p>
+                    </br>
                     <?php endif;?>
                     <?php if($member->elseSkills!=null):?>
-                    <p><strong>其他技能：</strong><?php echo e($member->elseSkills); ?></p>
+                    <h><strong>其他技能：</strong></h>
+                    </br>
+                    <div class="textarea"><?php echo e($member->elseSkills); ?></div>
+                    </br>
                     <?php endif;?>
                     <?php if($member->expectation!=null):?>
-                    <p><strong>自我预期：</strong><?php echo e($member->expectation); ?></p>
+                    <h><strong>自我预期：</strong></h>
+                    </br>
+                    <div class="textarea"><?php echo e($member->expectation); ?></textarea>
                     <?php endif;?>
                 </div>
             </div>
@@ -53,18 +66,6 @@
         }
     </script>
     <style>
-        .container{
-            position: relative;
-            margin-left: 2%;
-        }
-        .cardBox {
-            width: 100%;
-            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-            /*text-align: center;*/
-            float: left;
-            margin-right: 10px;
-            padding: 5px;
-            padding-top: 15px;
-        }
+
     </style>
 @endsection
